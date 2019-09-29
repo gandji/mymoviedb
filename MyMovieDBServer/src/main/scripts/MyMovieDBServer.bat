4b9d37baf3c340d7ed0ed48013f0e80197bb22b1
gandji <gandji@free.fr>
2019-09-29 09:58:04 -0400

    fix regressions

--- MyMovieDBServer/src/main/scripts/MyMovieDBServer.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-start "" javaw -Dspring.datasource.password=@datasource.password@ ^
- -Dspring.datasource.username=@datasource.username@ ^
- -Dspring.datasource.url=@datasource.url@ ^
- -Dmymoviedb.tmdb.apikey=@tmdb.apikey@ ^
- -jar @artifactId@-@version@.@packaging@ %*
