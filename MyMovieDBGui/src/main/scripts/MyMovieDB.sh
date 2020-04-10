3db2c641962a9b2f1f43b951e36ee16a15b967c6
gandji <gandji@free.fr>
2020-04-10 11:54:29 -0400

    prepare release

--- MyMovieDBGui/src/main/scripts/MyMovieDB.sh
+++ MyMovieDBGui/src/main/scripts/MyMovieDB.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 java -Dspring.datasource.password="@datasource.password@" \
  -Dspring.datasource.username=@datasource.username@ \
  -Dspring.datasource.url=@datasource.url@ \
